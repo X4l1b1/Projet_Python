2cd0f669874a0a70ca03d4e119d3f0587cb04f03
Arthur <arthur.passuello@heig-vd.ch>
2017-03-14 18:05:01 +0100

    Test

--- HW.py
+++ HW.py
@@ -1,14 +1,11 @@
-a = 5
-b = 8
-if a > 0:
-   
-    b += 1
-    # On affiche les valeurs des variables
-    print("a =",a,"et b =",b)
 
-def sum(a, b):
-	return a + b
 
-print(sum(a, b))
+print("Premier programme : ")
 
-print(sum(a, b))
+a = -1
+while(not(a > 0 and a < 100)):
+	a = input("Saisissez un chiffre entre 0 et 100 :")
+
+
+
+print("Merci")
