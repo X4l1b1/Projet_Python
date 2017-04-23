04df97b4cc1fa4cc047875c8ee8a23ca78249dbc
reds <roman.devuyst@gmail.com>
2017-04-23 19:00:17 +0200

    Update premiere interface

--- premiereinterface.py
+++ premiereinterface.py
@@ -1,27 +1,36 @@
-# -*-coding:Latin-1 -*
-import Tkinter as tk
+
 from Tkinter import *
+import ttk
 
-fenetre = Tk ()
+root = Tk()
 
-fenetre.configure(bg = "yellow")
+content = ttk.Frame(root)
+frame = ttk.Frame(content, borderwidth=5, relief="sunken", width=400, height=100)
+namelbl = ttk.Label(content, text="Name")
+name = ttk.Entry(content)
+
+onevar = BooleanVar()
+twovar = BooleanVar()
+threevar = BooleanVar()
+onevar.set(True)
+twovar.set(False)
+threevar.set(True)
+
+one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
+two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
+three = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
+ok = ttk.Button(content, text="Okay")
+cancel = ttk.Button(content, text="Cancel")
+
+content.grid(column=0, row=0)
+frame.grid(column=0, row=0, columnspan=3, rowspan=2)
+namelbl.grid(column=3, row=0, columnspan=2)
+name.grid(column=3, row=1, columnspan=2)
+one.grid(column=0, row=3)
+two.grid(column=1, row=3)
+three.grid(column=2, row=3)
+ok.grid(column=3, row=3)
+cancel.grid(column=4, row=3)
 
 
-fenetre_name = Label (fenetre, text = "Hello")
-fenetre_name.pack()
-
-boutton_Quit = Button (fenetre, text = "quitter la fenetre" , command = fenetre.quit)
-boutton_Quit.pack()
-
-boutton_PlusouMoins = Button (fenetre, text = "+/-" )
-boutton_PlusouMoins.pack()
-
-
-boutton_Agrandir_Fenetre = Button(fenetre, text='RESET', font='Arial -20 bold', relief='flat', bg='red', fg = 'blue', activebackground = "blue")
-boutton_Agrandir_Fenetre.pack()
-
-
-
-
-
-fenetre.mainloop ()
+root.mainloop()
